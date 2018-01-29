7808de0859ccf372c1973e6ad4f9aebe1d9f43f7
king-king <niuyiwangqun@sohu.com>
2018-01-29 17:25:27 +0800

    change

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,32 @@
 const gulp = require('gulp');
-const path=require('path');
+const path = require('path');
+const babel = require('gulp-babel');
+const rename = require('gulp-rename');
+
+let src = 'test/**/*.jsx';
 
 gulp.task('watch', function () {
     gulp.watch([
-        'src/**/*'
+        'test/**/*.jsx'
     ], function (e) {
         //any changed detected, call build task
+        src = path.dirname(e.path) + '/**/*.jsx'
         console.log(path.dirname(e.path));
     });
+});
+
+gulp.task('compile', function () {
+    return gulp.src(src)
+        .pipe(babel({
+            presets: ['react']
+        }))
+        .pipe(rename((p) => {
+            p.basename = 'main';
+            p.extname = '.js';
+        }))
+        .pipe(gulp.dest('test'))
+});
+
+gulp.task('default', function () {
+
 });
