61ba4b95fd1ce985448354ef3dba250814fcdf46
king-king <niuyiwangqun@sohu.com>
2018-01-29 18:29:09 +0800

    change

--- test/1/helloword.jsx
+++ test/1/helloword.jsx
@@ -1,4 +1,19 @@
+class MyHello extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            indexs: [0, 1, 2, 3, 4]
+        };
+    }
+    render() {
+        return (<div>{this.state.indexs.map((item) => {
+            return <div key={item}>{item}</div>;
+        })}</div>);
+    }
+}
+
+
 ReactDOM.render(
-    <h1>Hello,My World!</h1>,
+    <MyHello />,
     document.getElementById('example')
 );
--- test/1/main.js
+++ test/1/main.js
@@ -1,5 +1,23 @@
-ReactDOM.render(React.createElement(
-    'h1',
-    null,
-    'Hello,My World!'
-), document.getElementById('example'));
+class MyHello extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            indexs: [0, 1, 2, 3, 4]
+        };
+    }
+    render() {
+        return React.createElement(
+            'div',
+            null,
+            this.state.indexs.map(item => {
+                return React.createElement(
+                    'div',
+                    { key: item },
+                    item
+                );
+            })
+        );
+    }
+}
+
+ReactDOM.render(React.createElement(MyHello, null), document.getElementById('example'));
