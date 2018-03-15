47c73a1545d3d7872f1bee868847b81532a1b3ff
wangqun6 <wangqun28>
2018-03-15 18:45:15 +0800

    添加webpack-dev-server

--- src/static/js/test/main.jsx
+++ src/static/js/test/main.jsx
@@ -1,3 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Com extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div>hello</div>
+    }
+}
+
+ReactDOM.render(<Com></Com>, document.querySelector('#wrap'));
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,24 @@
 let config = require('./webpack.config');
 const path = require('path');
 
-console.log(JSON.stringify(config));
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
 
-config.devServer = {
-    contentBase: path.join(__dirname, "dist"),
-    port: 9000,
-    color: true
-};
 
-module.exports = config;
+config.output.publicPath = 'http://localhost:9000/';
+
+const compiler = webpack(config);
+const devServer = new WebpackDevServer(compiler, {
+    inline: true,
+    compress: true,
+    stats: {
+        chunks: false,
+        children: false,
+        colors: true
+    },
+    historyApiFallback: true,
+});
+console.log('new')
+devServer.listen(9000, 'localhost', function () {
+    console.log();
+});
