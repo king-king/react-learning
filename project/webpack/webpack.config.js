b1f23cd42c1ca6786d7f7381f37f11d0b1ea17c9
king-king <niuyiwangqun@sohu.com>
2018-04-02 13:37:16 +0800

    change

--- project/webpack/webpack.config.js
+++ project/webpack/webpack.config.js
@@ -4,13 +4,68 @@ const fs = require('fs');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production'; // 是否是发布版
 
 const entries = {
     libs: ['react', 'react-dom', 'redux', 'react-redux', 'redux-thunk', 'axios', 'prop-types']
 };
+
+// 组装插件
+function getPlugins() {
+    const plugins = [
+        new CleanWebpackPlugin('dist/*', {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'libs'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest'
+        })
+    ];
+    // 处理html
+    /* 过滤掉common文件夹 */
+    fs.readdirSync('src/view/').filter(m => m !== 'common').forEach((moduleName) => {
+        // 每个module内还有若干页面
+        fs.readdirSync(`src/view/${moduleName}`).forEach((page) => {
+            // 探查对应的js是否存在
+            const chunk = `${moduleName}/${page.replace('.html', '')}/main`;
+            const isE = fs.existsSync(`src/static/js/${moduleName}/${page.replace('.html', '')}/main.jsx`);
+            if (isE) {
+                entries[chunk] = path.resolve(__dirname, `../src/static/js/${moduleName}/${page.replace('.html', '')}/main.jsx`);
+            }
+            plugins.push(new HtmlWebpackPlugin({
+                // 模板为同级目录下的index.html，为何不用写路径，是因为默认上下文问webpack.config.js所在的文件夹
+                template: `src/view/${moduleName}/${page}/`,
+                // 自动生成HTML文件的名字,可以嵌套文件夹
+                filename: `view/${moduleName}/${page}`,
+                chunks: ['manifest', 'libs'].concat(isE ? [chunk] : [])
+            }));
+        });
+    });
+    plugins.push(new ExtractTextPlugin({
+        filename: `static/style/[name]${isProduction ? '_[contenthash:10]' : ''}.css`
+    }));
+    isProduction && plugins.push(new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            drop_console: true,
+            warnings: false,
+            collapse_vars: true,
+            reduce_vars: true
+        },
+        output: {
+            comments: false,
+            beautify: false
+        },
+        sourceMap: false
+    }));
+    return plugins;
+}
+
 const config = {
     entry: entries,
     output: {
@@ -52,62 +107,5 @@ const config = {
         }
     }
 };
-
-// 组装插件
-function getPlugins() {
-    const plugins = [
-        new CleanWebpackPlugin('dist/*', {
-            root: __dirname,
-            verbose: true,
-            dry: false
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'libs'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'manifest'
-        })
-    ];
-    // 处理html
-    fs.readdirSync('src/view/').filter(m => m !== 'common' /* 过滤掉common文件夹 */ ).forEach((moduleName) => {
-        // 每个module内还有若干页面
-        fs.readdirSync('src/view/' + moduleName).forEach((page) => {
-            // 探查对应的js是否存在
-            const chunk = moduleName + '_' + page.replace('.html', '');
-            const isE = fs.existsSync(`src/static/js/${moduleName}/${page.replace('.html', '')}/main.jsx`);
-            if (isE) {
-                entries[chunk] = path.resolve(__dirname, `../src/static/js/${moduleName}/${page.replace('.html', '')}/main.jsx`);
-            }
-            plugins.push(
-                new htmlWebpackPlugin({
-                    //模板为同级目录下的index.html，为何不用写路径，是因为默认上下文问webpack.config.js所在的文件夹
-                    template: `src/view/${moduleName}/${page}/`,
-                    //自动生成HTML文件的名字,可以嵌套文件夹
-                    filename: `view/${moduleName}/${page}`,
-                    chunks: ['manifest', 'libs'].concat(isE ? [chunk] : [])
-                })
-            );
-        });
-    });
-    plugins.push(new ExtractTextPlugin({
-        filename: `static/style/[name]${isProduction ? '_[contenthash:10]' : ''}.css`
-    }));
-    isProduction && plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                drop_console: true,
-                warnings: false,
-                collapse_vars: true,
-                reduce_vars: true
-            },
-            output: {
-                comments: false,
-                beautify: false
-            },
-            sourceMap: false
-        })
-    );
-    return plugins;
-}
 
 module.exports = config;
